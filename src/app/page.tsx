4334fc6af7a5f4d88c71a8d9b3ad4cdff265ad62
jeffrey <jefro.cab@gmail.com>
2025-02-09 17:06:55 -0400

    feat: instalacion y configuracion de librerias -taildwind -shadcn -transition-react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,30 @@
+'use client';
 import React from "react";
+import Main from "../../components/layout/Main";
 
 const HomePage: React.FC = () => {
+
   return (
     <>
-      <div className="mobile-only">
-        <div>
-          <h1 className="text-4xl font-bold text-white">Bienvenido a mi App</h1>
-          <p className="mt-4 text-lg text-white">
-            Esta es una aplicación solo para dispositivos móviles.
-          </p>
+      <Main className="portada-theme w-full h-screen">
+        <div className="max-w-2xl px-4">
+          <div className="mobile-only ">
+            <div>
+              <h1 className="mt-8">Bienvenido a mi App</h1>
+              <p className="mt-3 text-lg text-">
+                Esta e
+              </p>
+            </div>
+          </div>
+          <div className="desktop-message">
+            <div className="flex flex-col items-center justify-center min-h-screen">
+              <h1 className="text-4xl font-bold">
+                Esta aplicación solo está disponible en dispositivos móviles.
+              </h1>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="desktop-message">
-        <div className="flex flex-col items-center justify-center  min-h- bg-gray-800 text-white">
-          <h1 className="text-4xl font-bold">
-            Esta aplicación solo está disponible en dispositivos móviles.
-          </h1>
-        </div>
-      </div>
+      </Main>
     </>
   );
 };
